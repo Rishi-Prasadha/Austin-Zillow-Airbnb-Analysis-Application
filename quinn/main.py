9e950291bf7e42ed5fec73c39e92d508e46ab2ba
Quinn <q.stroube@gmail.com>
2022-08-01 20:49:31 -0500

    Initial Commit

---
+++ quinn/main.py
@@ -0,0 +1,24 @@
+from multiprocessing.context import assert_spawning
+import fire
+import questionary
+import sys
+from pathlib import Path
+import pandas as pd
+
+
+from utils.calculators import (
+    avg_price,
+    std
+)
+from utils.utils import (
+    enter_zip,
+    get_bath,
+    get_bed
+)
+
+
+def run():
+    zip = enter_zip()
+    bath = get_bath()
+    bed = get_bed()
+
